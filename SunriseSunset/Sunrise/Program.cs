170a8668c2add2a2cb49e63af1caf4f03b320e18
Adrian Stevens <adrian.stevens@gmail.com>
2018-05-02 18:55:51 -0700

    Added SunriseSunset sample

---
+++ SunriseSunset/Sunrise/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
+namespace MyBlinky
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Netduino.Foundation.Network.Initializer.InitializeNetwork();
+
+            Thread.Sleep(4000);
+
+            var sunrise = new SunriseService();
+
+            var data = sunrise.GetSunriseSunset(49.246, -123.1162);
+
+            Debug.Print(data.ToString());
+        }
+    }
+}
