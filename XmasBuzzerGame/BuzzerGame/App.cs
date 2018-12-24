2bdfe151a8dc24b505379bb865bf0280003800d4
Adrian Stevens <adrian.stevens@gmail.com>
2018-12-23 18:33:18 -0800

    Add Xmas Buzzer Game

---
+++ XmasBuzzerGame/BuzzerGame/App.cs
@@ -0,0 +1,127 @@
+using Microsoft.SPOT;
+using N = SecretLabs.NETMF.Hardware.Netduino;
+using Netduino.Foundation.Audio;
+using Netduino.Foundation.Sensors.Buttons;
+using Netduino.Foundation.Displays;
+using Netduino.Foundation.LEDs;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+
+namespace BuzzerGame
+{
+    public class App
+    {
+        float[] notes = new float[] { 261.63f, 329.63f, 392, 523.25f };
+
+        PushButton resetButton;
+        InputPort tree;
+
+        PwmLed redLed, greenLed;
+
+        PiezoSpeaker speaker;
+        GraphicsLibrary display;
+
+        //game variables
+        int count;
+        bool isPlaying = true;
+
+        public void Run()
+        {
+            InitializePeripherals();
+
+            UpdateDisplay();
+
+            greenLed.StartPulse(1000, 0.8f, 0.05f);
+
+            //thread to detect touches
+            var thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (isPlaying && tree.Read() == false)
+                    {
+                        OnTouched();
+                    }
+                    Thread.Sleep(10);
+                }
+            });
+            thread.Start();
+        }
+
+        private void OnTouched ()
+        {
+            count++;
+            PlayBuzzer();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    redLed.SetBrightness(0.8f);
+                    Thread.Sleep(25);
+                    redLed.SetBrightness(0);
+                    Thread.Sleep(25);
+                }
+            }).Start();
+
+            UpdateDisplay();
+        }
+
+        private void ResetGame ()
+        {
+            count = 0;
+        }
+
+        private void InitializePeripherals()
+        {
+            resetButton = new PushButton(N.Pins.ONBOARD_BTN, Netduino.Foundation.CircuitTerminationType.Floating);
+            resetButton.Clicked += OnResetButton;
+
+            tree = new InputPort(N.Pins.GPIO_PIN_D10, true, Port.ResistorMode.PullUp);
+
+            speaker = new PiezoSpeaker(N.PWMChannels.PWM_PIN_D3);
+            greenLed = new PwmLed(N.PWMChannels.PWM_PIN_D5, 2.0f);
+            redLed = new PwmLed(N.PWMChannels.PWM_PIN_D6, 2.0f);
+
+            var ssd1306 = new SSD1306(0x3c, 400, SSD1306.DisplayType.OLED128x64);
+            display = new GraphicsLibrary(ssd1306);
+            display.CurrentFont = new Font8x8();
+            
+            display.Clear(true);
+
+            PlayBuzzer();
+        }
+
+        private void OnResetButton(object sender, EventArgs e)
+        {
+            if(isPlaying == false)
+                ResetGame();
+
+            isPlaying = !isPlaying;
+
+            UpdateDisplay();
+        }
+
+        private void PlayBuzzer ()
+        {
+            speaker.PlayTone(notes[0], 50);
+        }
+
+        private void UpdateDisplay ()
+        {
+            display.Clear();
+
+            display.DrawText(0, 0, 0, "XMas Buzzer");
+
+            if (isPlaying)
+            {
+                display.DrawText(0, 20, 0, "Count: " + count);
+            }
+            else
+            {
+                display.DrawText(0, 20, 0, "** Paused **");
+            }
+            display.Show();
+        }
+    }
+}
